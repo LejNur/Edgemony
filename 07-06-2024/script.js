eaddb3ede6e4f737a2e0897458d97f647ab28c36
Lejla <lejla.nrk@gmail.com>
2024-06-07 16:08:16 +0200

    version without last two exercises

--- 07-06-2024/script.js
+++ 07-06-2024/script.js
@@ -1,21 +1,60 @@
 import { API_KEY } from "./keys.js";
 
+const containerEl = document.querySelector('.container');
+const searchBar = document.querySelector('.search-bar');
+
 
   fetch("https://api.themoviedb.org/3/movie/popular", {
     headers: {
       Authorization: `Bearer ${API_KEY}`,
     },
   })
-    .then((response) => response.json())
-    .then((response) => console.log(response))
+    .then((res) => res.json())
+
+    .then((response) => {
+      console.log(response);
+      const results = response.results;
+      renderList(results, containerEl);
+      console.log(results);
+
+      searchBar.addEventListener("input", (event) => {
+        const inputValue = event.target.value;
+        filterMovies(inputValue, results);
+      });
+    })
     .catch((err) => console.error(err));
 
     
-  // const options = {
-  //   method: "GET",
-  //   headers: {
-  //     accept: "application/json",
-  //     Authorization:
-  //       `Bearer ${API_KEY}`
-  //   },
-  // };
+//Making Card List
+  function renderList (res, container) {
+
+    res.forEach((element) => {
+      const card = document.createElement("div");
+      const cardImg = document.createElement("img");
+      const cardTitle = document.createElement("h2");
+      const cardOverview = document.createElement("p");
+
+      card.className = "card";
+      cardImg.className = 'card-img';
+      cardImg.src = ` https://image.tmdb.org/t/p/w1280${element.poster_path}`;
+      cardTitle.textContent = element.title;
+      cardOverview.textContent = element.overview;
+
+      container.append(card);
+      card.appendChild(cardImg);
+      card.appendChild(cardTitle);
+      card.appendChild(cardOverview);
+    });
+  }
+
+
+//Filtering movies with input
+function filterMovies (title, movies) {
+  const filterResult = movies.filter((movie) => {
+    return movie.title.toLowerCase().includes(title);
+  })
+
+  containerEl.innerHTML = '';
+  renderList(filterResult, containerEl);
+}
+
