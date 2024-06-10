b4f5a96759805091026ff0f54c97e7b3c007f406
Lejla <lejla.nrk@gmail.com>
2024-06-10 23:55:28 +0200

    style:card style and animation

--- 10-06-2024/renderList.js
+++ 10-06-2024/renderList.js
@@ -4,19 +4,23 @@ export function renderList(movie, container) {
   container.innerHTML = "";
   movie.forEach((element) => {
     const card = document.createElement("div");
-    const cardImg = document.createElement("img");
+    const cardContent = document.createElement("div");
+    // const cardImg = document.createElement("img");
     const cardTitle = document.createElement("h2");
     const cardOverview = document.createElement("p");
 
     card.className = "card";
-    cardImg.className = "card-img";
-    cardImg.src = `https://image.tmdb.org/t/p/w1280${element.poster_path}`;
+    card.style.backgroundImage = `url('https://image.tmdb.org/t/p/w1280${element.poster_path}')`;
+    // cardImg.className = "card-img";
+    // cardImg.src = `https://image.tmdb.org/t/p/w1280${element.poster_path}`;
+    cardContent.className = 'card-content';
     cardTitle.textContent = element.title ?? element.name;
     cardOverview.textContent = element.overview;
 
     container.append(card);
-    card.appendChild(cardImg);
-    card.appendChild(cardTitle);
-    card.appendChild(cardOverview);
+    card.appendChild(cardContent);
+    // card.appendChild(cardImg);
+    cardContent.appendChild(cardTitle);
+    cardContent.appendChild(cardOverview);
   });
 }
