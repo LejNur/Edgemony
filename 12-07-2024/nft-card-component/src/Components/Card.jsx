5cfcee7ba6f46725a7c7fff09e6bcc50b61011d7
Lejla <lejla.nrk@gmail.com>
2024-07-13 18:37:00 +0200

    style:responsive & active state

--- 12-07-2024/nft-card-component/src/Components/Card.jsx
+++ 12-07-2024/nft-card-component/src/Components/Card.jsx
@@ -1,11 +1,19 @@
 function Card() {
   return (
-    <div className="bg-cardBg w-1/5 p-6 rounded-2xl flex flex-col  shadow-mainBgShadow">
-      <img
-        src="images/image-equilibrium.jpg"
-        className="rounded-lg object-cover"
-      />
-      <h2 className="text-white text-2xl font-bold my-3">Equilibrium #3429</h2>
+    <div className="bg-cardBg w-full max-w-sm sm:w-2/4 p-6 m-4 sm:m-6 rounded-2xl flex flex-col  shadow-mainBgShadow">
+      <div className="relative">
+        <img
+          src="images/image-equilibrium.jpg"
+          className="rounded-lg object-cover"
+        />
+        <div className="absolute inset-0 flex justify-center items-center bg-cyan bg-opacity-40 opacity-0 cursor-pointer hover:opacity-100 transition-opacity">
+          <img src="images/icon-view.svg" alt="icon view" />
+        </div>
+      </div>
+
+      <h2 className="text-white text-2xl font-bold my-3 cursor-pointer hover:text-cyan">
+        Equilibrium #3429
+      </h2>
       <p className="text-softBlue text-lg font-regular tracking-wider my-4">
         Our Equilibrium collection promotes balance and calm.
       </p>
@@ -29,7 +37,10 @@ function Card() {
           className="w-6 border border-1 rounded-full border-white"
         />
         <p className="text-softBlue font-regular tracking-wider">
-          Creation of <span className="text-white font-bold">Jules Wyvern</span>
+          Creation of{" "}
+          <span className="text-white font-bold cursor-pointer hover:text-cyan">
+            Jules Wyvern
+          </span>
         </p>
       </div>
     </div>
