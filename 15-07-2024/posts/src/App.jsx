192031e15696a93cf2f6e0759a7155caffeb4d7e
Lejla <lejla.nrk@gmail.com>
2024-07-15 15:21:12 +0200

    feat:id to blog api

--- 15-07-2024/posts/src/App.jsx
+++ 15-07-2024/posts/src/App.jsx
@@ -1,13 +1,45 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0);
+  const [post, setPost] = useState({});
+  const [postId, setPostId] = useState(1);
+
+  function handleChange(e) {
+    setPostId(e.target.value);
+  }
+
+  useEffect(() => {
+    fetch(`https://jsonplaceholder.typicode.com/posts/${postId}`)
+      .then((res) => res.json())
+      .then((res) => setPost(res));
+  }, [postId]);
 
   return (
-    <>
-      <h1 className="text-lg text-lime-300">Hello</h1>
-    </>
+    <div className=" bg-slate-100 h-screen flex flex-col items-center gap-4 p-5 ">
+      <h1 className="text-5xl text-teal-500 font-bold">BlogPosts</h1>
+      <input
+        min="1"
+        type="number"
+        placeholder="Insert a number to find a blog"
+        className="w-60 bg-gray-50 border-2 border-teal-500 text-gray-900 text-sm rounded-lg p-2.5 shadow-sm"
+        onChange={handleChange}
+      />
+      <div className="w-96 my-5 p-4 flex flex-col justify-center items-start gap-2 border-2 border-teal-500 rounded-lg shadow-md">
+        <p>
+          <span className="text-gray-800 font-bold">Post ID: </span>
+          {post.id}
+        </p>
+        <p>
+          <span className="text-gray-800 font-bold">Title: </span>
+          {post.title}
+        </p>
+        <p>
+          <span className="text-gray-800 font-bold">Post: </span>
+          {post.body}
+        </p>
+      </div>
+    </div>
   );
 }
 
