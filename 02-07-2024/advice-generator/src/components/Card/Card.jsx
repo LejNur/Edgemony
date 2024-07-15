97b83a617bde62c23159ad8f51d607925bd7dbe1
Lejla <lejla.nrk@gmail.com>
2024-07-15 16:04:08 +0200

    feat:random advice-API & useEffect

--- 02-07-2024/advice-generator/src/components/Card/Card.jsx
+++ 02-07-2024/advice-generator/src/components/Card/Card.jsx
@@ -1,34 +1,53 @@
 import styles from "../Card/Card.module.css";
 import divider from "../../assets/svg/divider.svg";
 import dice from "../../assets/svg/dice.svg";
-import { advices } from "../../advice.js";
+// import { advices } from "../../advice.js";
 import { useEffect, useState } from "react";
 
 function Card() {
   const [text, setText] = useState({
     id: null,
-    advice: "Click a button to discover your advice of the day",
+    advice: "",
   });
 
   const [isAnimating, setIsAnimating] = useState(null);
 
-  function handleClick() {
-    const randomIndex = Math.floor(Math.random() * advices.length);
-    const randomAdvice = advices[randomIndex];
-    setText({ id: randomAdvice.id, advice: randomAdvice.advice });
-    setIsAnimating(true);
+  // function handleClick() {
+  //   const randomIndex = Math.floor(Math.random() * advices.length);
+  //   const randomAdvice = advices[randomIndex];
+  //   setText({ id: randomAdvice.id, advice: randomAdvice.advice });
+  //   setIsAnimating(true);
 
+  //   setTimeout(() => {
+  //     setIsAnimating(false);
+  //   }, 1000);
+  // }
+
+  function handleClick() {
+    setIsAnimating(true);
+    fetchData();
     setTimeout(() => {
       setIsAnimating(false);
     }, 1000);
   }
+
+  async function fetchData() {
+    const res = await fetch("	https://api.adviceslip.com/advice");
+    const resData = await res.json();
+    const advice = resData.slip;
+    setText({ id: advice.id, advice: advice.advice });
+  }
+
+  useEffect(() => {
+    fetchData();
+  }, []);
 
   return (
     <div className={styles.card}>
       {text.id && <h1 className={styles.title}>Advice #{text.id}</h1>}
       <p className={styles.paragraph}>{text.advice}</p>
       <img src={divider} alt="pattern-divider" />
-      <button className={styles.button} onClick={() => handleClick()}>
+      <button className={styles.button} onClick={handleClick}>
         <img
           src={dice}
           alt="icon-dice"
