82cdc6a9add7fbcd4e3a0f7b771662e70a2bf849
Lejla <lejla.nrk@gmail.com>
2024-07-16 15:40:18 +0200

    feat:saving facts to localStorage

--- 16-07-2024/exercise-localStorage/src/App.jsx
+++ 16-07-2024/exercise-localStorage/src/App.jsx
@@ -1,35 +1,51 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState, useEffect } from "react";
+import { API_KEY } from "./keys.js";
+
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [fact, setFact] = useState(null);
+  const [favFacts, setFavFacts] = useState(
+    JSON.parse(localStorage.getItem("randomFacts")) || []
+  );
+
+  async function getRandomFact() {
+    const results = await fetch("https://api.api-ninjas.com/v1/facts", {
+      method: "GET",
+      headers: {
+        "X-Api-Key": API_KEY,
+      },
+    });
+    const data = await results.json();
+    setFact(data[0].fact);
+  }
+
+  useEffect(() => {
+    getRandomFact();
+  }, []);
+
+  useEffect(() => {
+    toLocalStorage();
+  }, [favFacts]);
+
+  function handleClick() {
+    setFavFacts((prevFacts) => [...prevFacts, fact]);
+  }
+
+  function toLocalStorage() {
+    localStorage.setItem("randomFacts", JSON.stringify(favFacts));
+  }
+
+  console.log(JSON.parse(localStorage.getItem("randomFacts")));
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <h1>Here is your random fact of the day: </h1>
+      <h2>{fact}</h2>
+      <button onClick={getRandomFact}>Generate another random fact</button>
+      <button onClick={handleClick}>Save Fact</button>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
