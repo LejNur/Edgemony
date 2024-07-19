613e7904153421143876db8d382c168938806fa5
Lejla <lejla.nrk@gmail.com>
2024-07-19 18:08:11 +0200

    style:cart icon

--- 18-07-2024/react-ecommerce/src/Components/Navbar.jsx
+++ 18-07-2024/react-ecommerce/src/Components/Navbar.jsx
@@ -1,16 +1,27 @@
+import { useContext } from "react";
 import { Link } from "react-router-dom";
+import { CartContext } from "../Providers/CartContext";
 
 function Navbar() {
+  const { cart } = useContext(CartContext);
   return (
     <header>
       <nav className="flex items-center justify-between py-6 px-12 bg-cyan-600 ">
         <span className="font-extralight text-cyan-300 text-3xl">myShop</span>
-        <ul className="flex gap-3 items-start font-medium text-slate-200 text-xl">
+        <ul className="flex gap-3 items-center justify-center font-medium text-slate-200 text-xl">
           <li>
             <Link to="/">Home</Link>
           </li>
           <li>
-            <Link to="/cart">Cart</Link>
+            <Link to="/cart" className="relative flex items-center">
+              <span className="text-3xl ">
+                <ion-icon name="cart-outline"></ion-icon>
+              </span>
+
+              <span className="text-sm text-slate-200 absolute -top-3 -right-4 rounded-xl flex items-center justify-center w-5 h-5 bg-green-700">
+                {cart.length}
+              </span>
+            </Link>
           </li>
         </ul>
       </nav>
--- 18-07-2024/react-ecommerce/src/Pages/Cart.jsx
+++ 18-07-2024/react-ecommerce/src/Pages/Cart.jsx
@@ -5,7 +5,7 @@ import { CartContext } from "../Providers/CartContext";
 function Cart() {
   const { cart } = useContext(CartContext);
   return (
-    <div className="w-fit flex flex-wrap gap-6">
+    <div className="w-fit flex flex-wrap gap-6 px-12 py-6">
       {cart.map((item) => (
         <CartItem key={item.id} item={item} />
       ))}
