277e967b4162e906ab9ac513bbd368afb52e27cf
Lejla <lejla.nrk@gmail.com>
2024-07-19 16:53:13 +0200

    style:product details page

--- 18-07-2024/react-ecommerce/src/Pages/ProductDetails.jsx
+++ 18-07-2024/react-ecommerce/src/Pages/ProductDetails.jsx
@@ -18,10 +18,22 @@ function ProductDetails() {
   }, []);
 
   return (
-    <div>
-      <p>This product has an id: {id}</p>
-      <p>{product && product.title}</p>
-      <button onClick={() => addItem(product)}>Add to cart</button>
+    <div className="w-1/2 flex flex-col gap-2 items-start py-8 px-6 text-slate-600">
+      <p className="font-extralight">
+        This product has an id: <span className="font-semibold">{id}</span>
+      </p>
+      <p className="font-bold text-3xl">{product && product.title}</p>
+      <img className="w-1/2" src={product.thumbnail} alt="" />
+      <p>{product.description}</p>
+      <span>
+        rating: <span className="font-semibold">{product.rating}</span>
+      </span>
+      <button
+        className="border border-cyan-600 rounded-md py-2 px-4 hover:bg-cyan-600 hover:text-slate-200 transition ease-in-out"
+        onClick={() => addItem(product)}
+      >
+        Add to cart
+      </button>
     </div>
   );
 }
