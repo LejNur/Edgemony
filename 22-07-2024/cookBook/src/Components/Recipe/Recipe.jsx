fb670bb69888a37bfdba717f7c9fa5019bc3a0fa
Lejla <lejla.nrk@gmail.com>
2024-07-26 14:52:46 +0200

    style:thumbnail photo

--- 22-07-2024/cookBook/src/Components/Recipe/Recipe.jsx
+++ 22-07-2024/cookBook/src/Components/Recipe/Recipe.jsx
@@ -3,7 +3,13 @@ import { Link } from "react-router-dom";
 function Recipe({ recipe }) {
   return (
     <tr>
-      <td className="whitespace-nowrap px-4 py-2 text-gray-700">{recipe.id}</td>
+      <td className="whitespace-nowrap px-4 py-2 text-gray-700">
+        <img
+          src={recipe.images.small}
+          alt={`thumbnail image of a ${recipe.name}`}
+          className="w-20 aspect-video object-cover"
+        />
+      </td>
       <td className="whitespace-nowrap px-4 py-2 font-medium text-gray-900">
         {recipe.name}
       </td>
@@ -13,16 +19,18 @@ function Recipe({ recipe }) {
       <td className="whitespace-nowrap px-4 py-2 text-gray-700">
         {recipe.cooktime}
       </td>
-      <td className="flex gap-2">
+      <td>
         <Link
           to={`/recipe/${recipe.id}`}
-          className="inline-block rounded bg-indigo-600 px-4 py-2 text-xs font-medium text-white hover:bg-indigo-700"
+          className=" rounded bg-indigo-600 px-4 py-2 text-xs font-medium text-white hover:bg-indigo-700"
         >
           Details
         </Link>
+      </td>
+      <td>
         <Link
           to={`/edit/${recipe.id}`}
-          className="inline-block rounded bg-indigo-600 px-4 py-2 text-xs font-medium text-white hover:bg-indigo-700"
+          className=" rounded bg-indigo-600 px-4 py-2 text-xs font-medium text-white hover:bg-indigo-700"
         >
           Edit
         </Link>
