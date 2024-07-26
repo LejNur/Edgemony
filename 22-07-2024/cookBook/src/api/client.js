9390f8a5bb1ec2ca8c187394766b139b8041c03d
Lejla <lejla.nrk@gmail.com>
2024-07-26 10:24:37 +0200

    feat: npm install --save react-toastify Toaster

--- 22-07-2024/cookBook/src/api/client.js
+++ 22-07-2024/cookBook/src/api/client.js
@@ -1,17 +1,4 @@
-// const data = [
-//   {
-//     id: "1",
-//     title: "Spaghetti",
-//     category: "Main Course",
-//     time: "20min",
-//   },
-//   {
-//     id: "2",
-//     title: "Pancakes",
-//     category: "Breakfast",
-//     time: "2-3min",
-//   },
-// ];
+
 import axios from "axios";
 const BASE_URL = "http://localhost:5000/recipes";
 
@@ -29,16 +16,7 @@ export async function addRecipe(body) {
   const res = await axios.post(BASE_URL, body);
   return res.data;
 }
-// export const addRecipe = (body) => {
-//   return new Promise((resolve) => {
-//     setTimeout(() => {
-//       resolve({
-//         ...body,
-//         id: self.crypto.randomUUID(),
-//       });
-//     }, 1000);
-//   });
-// };
+
 
 // export const editRecipe = (body) => {
 //   return new Promise((resolve) => {
