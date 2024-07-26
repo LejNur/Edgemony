f798a8bd5b0e36d3e1c8044638e1b62203238776
Lejla <lejla.nrk@gmail.com>
2024-07-26 19:52:26 +0200

    test

--- 22-07-2024/cookBook/src/pages/RecipeDetails.jsx
+++ 22-07-2024/cookBook/src/pages/RecipeDetails.jsx
@@ -33,7 +33,7 @@ function RecipeDetails() {
   if (isLoading) return <Skeleton />;
 
   return (
-    <div className="relative isolate overflow-hidden bg-creamyWhite px-6 py-24 sm:py-32 lg:overflow-visible lg:px-0">
+    <div className="relative isolate overflow-hidden bg-creamyWhite px-96 py-24 sm:py-32 lg:overflow-visible lg:px-0">
       <div className="mx-auto grid max-w-2xl grid-cols-1 gap-x-8 gap-y-16 lg:mx-0 lg:max-w-none lg:grid-cols-2 lg:items-start lg:gap-y-10">
         <div className="lg:col-span-2 lg:col-start-1 lg:row-start-1 lg:mx-auto lg:grid lg:w-full lg:max-w-7xl lg:grid-cols-2 lg:gap-x-8 lg:px-8">
           <div className="lg:pr-4">
@@ -65,8 +65,12 @@ function RecipeDetails() {
             </div>
           </div>
         </div>
-        <div className="-ml-12 -mt-12 p-12 lg:sticky lg:top-4 lg:col-start-2 lg:row-span-2 lg:row-start-1 lg:overflow-hidden">
-          <img alt="" src={recipe.images.large} className="w-full rounded-md" />
+        <div className="-ml-12 -mt-12 w-full lg:sticky lg:top-4 lg:col-start-2 lg:row-span-2 lg:row-start-1 lg:overflow-hidden">
+          <img
+            alt={recipe.name}
+            src={recipe.images.large}
+            className="w-4/5 mx-auto rounded-md"
+          />
         </div>
       </div>
     </div>
