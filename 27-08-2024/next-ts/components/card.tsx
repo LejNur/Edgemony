1a730346332bad52a55e47d79204d063236cd6dd
Lejla <lejla.nrk@gmail.com>
2024-08-29 18:35:17 +0200

    chore:blog title to route with id

--- 27-08-2024/next-ts/components/card.tsx
+++ 27-08-2024/next-ts/components/card.tsx
@@ -1,4 +1,5 @@
 import { IBlogPost } from "@/app/model/post";
+import Link from "next/link";
 
 // { post }: { post: BlogPost } destructures the post prop and defines its type as BlogPost!
 export default function Card({ post }: { post: IBlogPost }) {
@@ -9,9 +10,9 @@ export default function Card({ post }: { post: IBlogPost }) {
         className="overflow-hidden rounded-lg shadow transition hover:shadow-lg dark:shadow-gray-700/25"
       >
         <div className="bg-zinc-100 p-4 sm:p-6 ">
-          <a href="/post">
+          <Link href={`/post/${post.id}`}>
             <h3 className="mt-0.5 text-lg text-gray-900 ">{post.title}</h3>
-          </a>
+          </Link>
 
           <p className="mt-2 line-clamp-3 text-sm/relaxed text-gray-500 ">
             {post.body}
